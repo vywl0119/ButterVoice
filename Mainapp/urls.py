7abe8172405addabf4552651592fa603ccda71ae
정수빈 <vywl0119@naver.com>
2022-04-13 10:40:16 +0900

    url 연결 및 view수정

--- Mainapp/urls.py
+++ Mainapp/urls.py
@@ -5,5 +5,9 @@ from . import views
 app_name = 'Mainapp'
 
 urlpatterns = [
-
+    path('cu_call/', views.cu_call),
+    path('co_call/', views.co_call),
+    path('cu_main/', views.cu_main),
+    path('co_main/', views.co_main),
+    path('star/', views.star),
 ]
