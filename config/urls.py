13453b3c98f40b5e24d22192a4aa0e21cf834bbd
boooyeon <boooyeon@gmail.com>
2022-04-13 11:00:02 +0900

    static 경로 설정

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 from django.conf import settings
 
 
@@ -24,3 +25,4 @@ urlpatterns = [
     path('Board/', include('Boardapp.urls')),
     path('Home/', include('Homeapp.urls')),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
