61ccdb9b96e0563923cb77a600b8fa2eb93c3e86
정수빈 <vywl0119@naver.com>
2022-04-13 10:43:54 +0900

    Main views 수정

--- Mainapp/views.py
+++ Mainapp/views.py
@@ -3,16 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 def cu_call(request):
-    return render(request, 'Home/cu_call.html')
+    return render(request, 'Main/cu_call.html')
 
 def co_call(request):
-    return render(request, 'Home/co_call.html')
+    return render(request, 'Main/co_call.html')
 
 def cu_main(request):
-    return render(request, 'Home/cu_main.html')
+    return render(request, 'Main/cu_main.html')
 
 def co_main(request):
-    return render(request, 'Home/co_main.html')
+    return render(request, 'Main/co_main.html')
 
 def star(request):
-    return render(request, 'Home/star.html')
+    return render(request, 'Main/star.html')
