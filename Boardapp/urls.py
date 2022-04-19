377e0c3d34cd76a5c2a0e178ac6f2b15ca4b59dc
정수빈 <vywl0119@naver.com>
2022-04-19 16:11:07 +0900

    board url 연결 및 상담사, 고객 정보 표시

--- Boardapp/urls.py
+++ Boardapp/urls.py
@@ -7,8 +7,7 @@ app_name = 'Boardapp'
 urlpatterns = [
     path('co_detail/', views.co_detail, name='co_detail'),
     path('cu_detail/', views.cu_detail, name='cu_detail'),
-    path('board/', views.board, name='board'),
-    path('co_board/', views.co_board, name='co_board'),
-    path('cu_board/', views.cu_board,  name='cu_board'),
+    path('board/<str:type>', views.board, name='board'),
+
     
 ]
